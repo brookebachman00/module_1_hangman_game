041af36ee18d8558605b43bbe7984db8202382d5
github username <github email address>
2019-12-30 16:05:29 -0800

    migrate

--- app/models/playerword.rb
+++ app/models/playerword.rb
@@ -1,8 +1,5 @@
-class CreatePlayerwords< ActiveRecord::Migration[5.2]
-    def change
-        create_table: playerwords do |t|
-            t.string :player
-            t.string :word
-        end
-    end
+class Playerwords < ActiveRecord::Base
+    has_many :players
+    has_many :words
+
 end
