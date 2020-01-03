29f562fa00224394a5b5e42683be27ea1ef913eb
github username <github email address>
2020-01-03 10:28:16 -0800

    more Words

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ player3 = Player.create(name: "CCCCC")
 player4 = Player.create(name: "DDDDD")
 player5 = Player.create(name: "EEEEE")
  
-arr = ["horse", "birth", "mouse", "apple", "black", "white", "water", "users", "comes", "yummy", "hyper", "cards", "blink", "stick", "every", "using", "trust", "pluck", "trees", "proud"]
+arr = ["horse", "birth", "mouse", "apple", "black", "white", "water", "users", "comes", "yummy", "hyper", "cards", "blink", "stick", "every", "using", "trust", "pluck", "trees", "proud", "smile", "sadly", "dizzy", "junky", "quick", "juicy", "field", "unzip", "japan", "kicky", "amaze", "check", "major", "joyed", "unfix", "zebra"]
 arr.each do |ele|
     word = Word.create(word: ele)
 end
